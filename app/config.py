35db93a0bb3640517555ad67883603783f5053c1
Code-Slaves <115467630+Code-Slaves@users.noreply.github.com>
2024-04-08 09:35:53 +0200

    Delete app directory

--- app/config.py
+++
@@ -1,5 +0,0 @@
-class Configuration:
-	build_dotenv()
-	TOKEN = "7191354663:AAH067pY8k24axknQNSTpyhxu2A3ZfEwZ_k"
-	TOKEN_CONTRACT = "KT1Hc6KSHp3pLLiCXUkuHva62PRJQ8JmyAcP"
-	ANIMATION_PATH = "https://iroh22zbyhnsd6p4n2xjoh3h3jokn3crsl6tf2poovear4ukkwaa.arweave.net/RFx9ayHB2yH5_G6ulx9n2lym7FGS_TLp7nVICPKKVYA"
--- app/core/__init__.py
+++
@@ -1,3 +0,0 @@
-from core.aiostorage import ChatStorage
-from core.core import Token
-from core.cache_manager import CacheManager
